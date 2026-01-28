a54ea4f1aaa65b638bcd1598c3ed847f456ad8cc
mako00027 <kiro73297@gmail.com>
2026-01-28 13:17:33 +0200

    asdv

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
 
-  base:"/nvsH2983/",
+  base:"/sea/",
   server: {
     host: "::",
     port: 8080,
